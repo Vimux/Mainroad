99ee259106b9bc6f16ecce2b25cf58eb245c976a
weru <fromweru@gmail.com>
2023-01-18 21:45:38 +0300

    add deeplink helper functions
    Signed-off-by: weru <fromweru@gmail.com>

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,7 @@ const dark = 'dark';
 const active = 'active';
 let auto = 'auto';
 const storage_key = `{{ lower site.Title }}-color-mode`;
+const copied_text = `{{ T "copied" }}`;
 const key = `--color-mode`;
 const data = 'data-mode';
 const bank = window.localStorage;
@@ -55,8 +56,6 @@ function showLightingModeIcon() {
     current_mode = user_selected_mode ? user_selected_mode : getSystemMode();
   }
 
-  console.log(current_mode);
-
   elems('svg', lighting_mode_toggle).forEach(icon => {
     icon.dataset.mode === current_mode ? pushClass(icon, active) : deleteClass(icon, active);
   })
@@ -78,8 +77,85 @@ function setUserColorMode(manual = false) {
 
 setUserColorMode(); // kicks in immediately
 
+function addDeepLinks() {
+  let headingNodes = [], results, link, icon, current, id,
+  tags = ['h2', 'h3', 'h4', 'h5', 'h6'];
+
+  current = document.URL;
+
+  tags.forEach(function(tag){
+    results = document.getElementsByTagName(tag);
+    Array.prototype.push.apply(headingNodes, results);
+  });
+
+  console.log(tags);
+
+  function sanitizeURL(url) {
+    // removes any existing id on url
+    const hash = '#';
+    const positionOfHash = url.indexOf(hash);
+    if(positionOfHash > -1 ) {
+      const id = url.substr(positionOfHash, url.length - 1);
+      url = url.replace(id, '');
+    }
+    return url
+  }
+
+  headingNodes.forEach(function(node){
+    link = createEl('a');
+    icon = createEl('img');
+    icon.src = '{{ absURL "img/link.svg" }}';
+    link.className = 'link icon';
+    link.appendChild(icon);
+    id = node.getAttribute('id');
+    if(id) {
+      link.href = `${sanitizeURL(current)}#${id}`;
+      node.appendChild(link);
+      pushClass(node, 'link__owner');
+    }
+  });
+}
+
+function copyFeedback(parent) {
+  const copyText = document.createElement('div');
+  const yanked = 'link__copied';
+  copyText.classList.add(yanked);
+  copyText.innerText = copied_text;
+  if(!elem(`.${yanked}`, parent)) {
+    const icon = parent.getElementsByTagName('img')[0];
+    const originalSrc = icon.src;
+    icon.src = '{{ absURL "img/check.svg" }}';
+    parent.appendChild(copyText);
+    setTimeout(function() {
+      parent.removeChild(copyText)
+      icon.src = originalSrc;
+    }, 1500);
+  }
+}
+
+function copyDeepLinks() {
+  let deeplink, deeplinks, newLink, parent, target;
+  deeplink = 'link';
+  deeplinks = elems(`.${deeplink}`);
+  if(deeplinks) {
+    document.addEventListener('click', function(event)
+    {
+      target = event.target;
+      parent = target.parentNode;
+      if (target && containsClass(target, deeplink) || containsClass(parent, deeplink)) {
+        event.preventDefault();
+        newLink = target.href != undefined ? target.href : target.parentNode.href;
+        copyToClipboard(newLink);
+        target.href != undefined ?  copyFeedback(target) : copyFeedback(target.parentNode);
+      }
+    });
+  }
+}
+
 window.addEventListener('load', () => {
   initializeMenu();
+  addDeepLinks();
+  copyDeepLinks();
 
   doc.addEventListener('click', function(event) {
     let target = event.target;
